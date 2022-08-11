5bfc2488939e661ee9ec41645e65975a983d6d74
afgmlff <aeciofgm@gmail.com>
2022-08-10 21:00:52 -0300

    selected word randomized and color correction

--- wordle/src/app/words/words.component.ts
+++ wordle/src/app/words/words.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { wordlist } from './test';
 
 
@@ -6,15 +6,26 @@ import { wordlist } from './test';
 @Component({ //decorator, passa um obj de metadados
   selector: 'app-words', //tag html que uso pra representar o componente
   templateUrl: './words.component.html', 
-  styleUrls: ['./words.component.css']
+  styleUrls: ['./words.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class WordsComponent implements OnInit {
   test: number
+  rndWord: string
   constructor() { 
 
     const listLength = wordlist.length
     console.log("size: " + listLength)
     this.test = listLength
+
+    /*
+
+    filtrar p 5 letter words.
+
+    */
+
+    this.rndWord = wordlist[Math.floor(Math.random()*this.test)]
+
   }
 
   ngOnInit(): void {
